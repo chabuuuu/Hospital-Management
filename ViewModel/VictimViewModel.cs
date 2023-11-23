641e1ab4fc1f851d369be1c04dee57e02ffd2b18
Tam-Vu <vuthanhtam12062003@gmail.com>
2023-11-23 11:03:51 +0700

    thêm y sĩ và bệnh nhân

--- ViewModel/VictimViewModel.cs
+++ ViewModel/VictimViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,6 @@ namespace LTTQ_DoAn.ViewModel
 {
     public class VictimViewModel: BaseViewModel
     {
+
     }
 }
