7847918bb68b28693b1055f250c90b0ba889913b
Tam-Vu <vuthanhtam12062003@gmail.com>
2023-11-27 17:12:25 +0700

    thêm khoa (field), phòng bệnh (room), dịch vụ (services)

--- View/field.xaml.cs
+++ View/field.xaml.cs
@@ -20,9 +20,18 @@ namespace LTTQ_DoAn.View
     /// </summary>
     public partial class field : UserControl
     {
+        List<string> fields = new List<string>();
         public field()
         {
             InitializeComponent();
+            fields = new List<string>() { "khoa tim mạch", "khoa da liễu", "khoa ngoại", "khoa tiêu hóa", "khoa tiết niệu", 
+                "khoa nội thần kinh", "khoa ngoại thần kinh", "nhãn khoa", "khoa nhi", "khoa tai-mũi-họng", "khoa nam", "phụ khoa",
+                "khoa nội thần kinh", "khoa ngoại thần kinh", "nhãn khoa", "khoa nhi", "khoa tai-mũi-họng", "khoa nam", "phụ khoa"
+                ,"khoa tim mạch", "khoa da liễu", "khoa ngoại", "khoa tiêu hóa", "khoa tiết niệu",
+                "khoa nội thần kinh", "khoa ngoại thần kinh", "nhãn khoa", "khoa nhi", "khoa tai-mũi-họng", "khoa nam", "phụ khoa",
+                "khoa nội thần kinh", "khoa ngoại thần kinh", "nhãn khoa", "khoa nhi", "khoa tai-mũi-họng", "khoa nam", "phụ khoa"};
+            FieldList.ItemsSource = fields;
         }
+        
     }
 }
--- ViewModel/ServicesViewModel.cs
+++ ViewModel/ServicesViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LTTQ_DoAn.ViewModel
 {
-    public class DoctorAndNurseViewModel : BaseViewModel
+    public class ServicesViewModel : BaseViewModel
     {
     }
 }
