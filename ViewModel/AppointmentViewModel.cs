771197299acfa24e0f8a45badee6a383837291dd
Tam-Vu <vuthanhtam12062003@gmail.com>
2023-12-10 20:42:21 +0700

    làm thêm vài commnad trong khối appointment

--- ViewModel/AppointmentViewModel.cs
+++ ViewModel/AppointmentViewModel.cs
@@ -12,6 +12,8 @@ namespace LTTQ_DoAn.ViewModel
 {
     public class AppointmentViewModel : BaseViewModel
     {
+        public ICommand ViewAppointmentCommand { get; }
+        public ICommand ChangeAppointmentCommand { get; }
         public ICommand AddApointmentCommand { get; }
         public ICommand DeleteApointmentCommand { get; }
 
@@ -20,6 +22,8 @@ namespace LTTQ_DoAn.ViewModel
             // dựa vào class ViewModelCommand đã được định nghĩa
             AddApointmentCommand = new ViewModelCommand(ExecuteAddCommand, CanExecuteAddCommand);
             DeleteApointmentCommand = new ViewModelCommand(ExecuteDeleteCommand, CanExecuteDeleteCommand);
+            ViewAppointmentCommand = new ViewModelCommand(ExecuteViewCommand, CanExecuteViewCommand);
+            ChangeAppointmentCommand = new ViewModelCommand(ExecuteChangeCommand, CanExecuteChangeCommand);
         }
 
         //tham số thứ 1 là điều kiện thực hiện command
@@ -46,6 +50,33 @@ namespace LTTQ_DoAn.ViewModel
         private void ExecuteDeleteCommand(object? obj)
         {
 
+        }
+
+        private bool CanExecuteViewCommand(object? obj)
+        {
+            return true;
+        }
+        //tham số thứ 2 là hành động
+        private void ExecuteViewCommand(object? obj)
+        {
+            ViewAppointment wd = new ViewAppointment();
+            //cài mainwindow thành cửa số mới mở này để chút nữa đóng lại thì ta chỉ cần dùng lệnh close cho mainwindow
+            // vi dụ nút cancel ở trong AddAppointmentViewModel.cs
+            Application.Current.MainWindow = wd;
+            wd.ShowDialog();
+        }
+        private bool CanExecuteChangeCommand(object? obj)
+        {
+            return true;
+        }
+        //tham số thứ 2 là hành động
+        private void ExecuteChangeCommand(object? obj)
+        {
+            ChangeAppointment wd = new ChangeAppointment();
+            //cài mainwindow thành cửa số mới mở này để chút nữa đóng lại thì ta chỉ cần dùng lệnh close cho mainwindow
+            // vi dụ nút cancel ở trong AddAppointmentViewModel.cs
+            Application.Current.MainWindow = wd;
+            wd.ShowDialog();
         }
     }
 }
