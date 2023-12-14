3f72f06e1fb458c133f962d166f1a6a7ae307765
NgCgThanh <thanhhsnvlxd2000@gmail.com>
2023-12-14 09:13:26 +0700

    update phan feature_hover

--- ViewModel/VictimViewModel/AddVictimViewModel.cs
+++ ViewModel/VictimViewModel/AddVictimViewModel.cs
@@ -8,12 +8,90 @@ using System.Windows.Input;
 using System.Windows;
 using LTTQ_DoAn.Model;
 using LTTQ_DoAn.Repositories;
+using LTTQ_DoAn.View;
 
 namespace LTTQ_DoAn.ViewModel {
     public class AddVictimViewModel : BaseViewModel {
+        QUANLYBENHVIENEntities _db = new QUANLYBENHVIENEntities();
+        private int id;
         public ICommand CancelCommand { get; }
         public ICommand ConfirmAddCommand { get; }
+        public int Id { get => id; set => id = value; }
+        public string Ten {
+            get => ten; set {
+                ten = value;
+                OnPropertyChanged(nameof(Ten));
+            }
+        }
+        public string Ngaysinh {
+            get => ngaysinh; set {
+                ngaysinh = value;
+                OnPropertyChanged(nameof(Ngaysinh));
+            }
+        }
+        public string Gioitinh {
+            get => gioitinh; set {
+                gioitinh = value;
+                OnPropertyChanged(nameof(Gioitinh));
+            }
+        }
+        public string Bhyt {
+            get => bhyt; set {
+                bhyt = value;
+                OnPropertyChanged(nameof(Bhyt));
+            }
+        }
+        public string Maphong {
+            get => maphong; set {
+                maphong = value;
+                OnPropertyChanged(nameof(Maphong));
+            }
+        }
+        public string Ngaynhapvien {
+            get => ngaynhapvien; set {
+                ngaynhapvien = value;
+                OnPropertyChanged(nameof(Ngaynhapvien));
+            }
+        }
+        public string Diachi {
+            get => diachi; set {
+                diachi = value;
+                OnPropertyChanged(nameof(Diachi));
+            }
+        }
+
+        private string ten = null;
+        private string ngaysinh = null;
+        private string gioitinh = null;
+        private string bhyt = null;
+        private string maphong = null;
+        private string ngaynhapvien = null;
+        private string diachi = null;
+        public AddVictimViewModel(int id) {
+            this.Id = id;
+            CancelCommand = new ViewModelCommand(ExecuteCancelCommand, CanExecuteCancelCommand);
+            ConfirmAddCommand = new ViewModelCommand(ExecuteAddCommand, CanExecuteAddCommand);
+        }
+        public void insert() {
+            BENHNHAN newBenhnhan = new BENHNHAN() {
+                HOTEN = this.Ten,
+                GIOITINH = Gioitinh,
+                NGAYSINH = DateTime.Parse(Ngaysinh),
+                MABHYT = Bhyt,
+                MAPHONG = this.convertSUB_ID(Maphong),
+                NGAYNHAPVIEN = DateTime.Parse(Ngaynhapvien),
+                DIACHI = Diachi
+            };
+            newBenhnhan.MAPHONG = 1;
+            //MessageBox.Show(Gioitinh);
+            //MessageBox.Show(Ten + Gioitinh + Ngaysinh + Bhyt + this.convertSUB_ID(Maphong) + Ngaynhapvien + Diachi);
+            _db.BENHNHAN.Add(newBenhnhan);
+            //_db.BENHNHAN.AddObject(newBenhnhan);
+            _db.SaveChanges();
+            //BaseViewModel.global_db = _db;
+        }
         public AddVictimViewModel() {
+
             CancelCommand = new ViewModelCommand(ExecuteCancelCommand, CanExecuteCancelCommand);
             ConfirmAddCommand = new ViewModelCommand(ExecuteAddCommand, CanExecuteAddCommand);
         }
@@ -25,12 +103,27 @@ namespace LTTQ_DoAn.ViewModel {
         private bool CanExecuteCancelCommand(object? obj) {
             return true;
         }
+        public int convertSUB_ID(string Sub_id) {
+            // Chuỗi cần tách
+            string inputString = Sub_id;
 
+            // Tách các ký tự còn lại thành một chuỗi riêng
+            string remainingCharacters = inputString.Substring(3);
+
+            return int.Parse(remainingCharacters);
+        }
         //hành động thêm vào
         private void ExecuteAddCommand(object? obj) {
             //câu lệnh thêm ở đây
+            try {
+                insert();
+                MessageBox.Show("Thêm bệnh nhân mới thành công!");
+                Application.Current.MainWindow.Close(); // sau khi thêm sẽ đóng cửa sổ
 
-            Application.Current.MainWindow.Close(); // sau khi thêm sẽ đóng cửa sổ
+            } catch (Exception err) {
+                MessageBox.Show(err.Message);
+                Application.Current.MainWindow.Close(); // sau khi thêm sẽ đóng cửa sổ
+            }
         }
         //điều kiện để lệnh thêm được thực hiện: lich khám không có sẵn trong database
         private bool CanExecuteAddCommand(object? obj) {
--- ViewModel/VictimViewModel/MainVictimViewModel.cs
+++ ViewModel/VictimViewModel/MainVictimViewModel.cs
@@ -18,11 +18,25 @@ namespace LTTQ_DoAn.ViewModel {
         public ICommand AddCommand { get; }
         public ICommand DeleteCommand { get; }
         private List<BENHNHAN> victims;
+        private BENHNHAN selectedItem = null;
 
-        QUANLYBENHVIENEntities _db = new QUANLYBENHVIENEntities();
+        QUANLYBENHVIENEntities _db;
 
-        public List<BENHNHAN> Victims { get => victims; set => victims = value; }
+        public List<BENHNHAN> Victims {
+            get => victims; set {
+                victims = value;
+                OnPropertyChanged(nameof(Victims));
+            }
+        }
+        public BENHNHAN SelectedItem {
+            get => selectedItem; set {
+                selectedItem = value;
+                OnPropertyChanged(nameof(SelectedItem));
+            }
+        }
+
         private void Load() {
+            _db = new QUANLYBENHVIENEntities();
             Victims = _db.BENHNHAN.ToList();
             //System.Windows.MessageBox.Show("Done");
         }
@@ -40,13 +54,25 @@ namespace LTTQ_DoAn.ViewModel {
         }
         //tham số thứ 2 là hành động
         private void ExecuteAddCommand(object? obj) {
+            //MessageBox.Show(this.selectedItem.SUB_ID.ToString());
             AddVictim wd = new AddVictim();
+            wd.Closed += AddVictim_Closed;
             //cài mainwindow thành cửa số mới mở này để chút nữa đóng lại thì ta chỉ cần dùng lệnh close cho mainwindow
             // vi dụ nút cancel ở trong AddAppointmentViewModel.cs
             Application.Current.MainWindow = wd;
             wd.ShowDialog();
         }
-
+        private void AddVictim_Closed(object sender, EventArgs e) {
+            /*
+            if (BaseViewModel.global_db.BENHNHAN.Count() > 0)
+            {
+                _db = BaseViewModel.global_db;
+                // Xử lý sau khi cửa sổ AddVictim đã đóng
+                // Điều này có thể là nơi bạn thực hiện các hành động sau khi cửa sổ đã đóng
+                Victims = BaseViewModel.global_db.BENHNHAN.ToList();
+            }*/
+            Load();
+        }
         //tham số 1 điều kiện để xóa lịch khám
         private bool CanExecuteDeleteCommand(object? obj) {
             return true;
@@ -66,6 +92,7 @@ namespace LTTQ_DoAn.ViewModel {
             // vi dụ nút cancel ở trong AddAppointmentViewModel.cs
             Application.Current.MainWindow = wd;
             wd.ShowDialog();
+            Load();
         }
         private bool CanExecuteChangeCommand(object? obj) {
             return true;
