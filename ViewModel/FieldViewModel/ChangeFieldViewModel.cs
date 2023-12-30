40536d23950f5a3d143a4a87e3d33dd7464eb7fd
chabuuuu <87564111+chabuuuu@users.noreply.github.com>
2023-12-30 17:51:01 +0700

    finish change field

--- ViewModel/FieldViewModel/ChangeFieldViewModel.cs
+++ ViewModel/FieldViewModel/ChangeFieldViewModel.cs
@@ -8,6 +8,11 @@ using System.Windows;
 using LTTQ_DoAn.Model;
 using LTTQ_DoAn.Repositories;
 using System.Windows.Forms;
+using LTTQ_DoAn.View;
+using System.Windows.Media.Imaging;
+using Json.Net;
+using RestSharp;
+using System.Net.NetworkInformation;
 
 namespace LTTQ_DoAn.ViewModel
 {
@@ -15,11 +20,15 @@ namespace LTTQ_DoAn.ViewModel
     {
         QUANLYBENHVIENEntities _db = new QUANLYBENHVIENEntities();
 
-        private string bacsi;
+        private string bacsi = null;
+        private string image_url;
+        private BitmapImage image;
         private List<string> bacsiList;
         private KHOA khoa;
         public ICommand CancelCommand { get; }
         public ICommand ConfirmChangeCommand { get; }
+        public ICommand AddImageCommand { get; }
+
         public KHOA Khoa
         {
             get => khoa; set
@@ -43,6 +52,24 @@ namespace LTTQ_DoAn.ViewModel
             {
                 bacsiList = value;
                 OnPropertyChanged(nameof(BacsiList));
+            }
+        }
+
+        public BitmapImage Image
+        {
+            get => image; set
+            {
+                image = value;
+                OnPropertyChanged(nameof(Image));
+            }
+        }
+
+        public string Image_url
+        {
+            get => image_url; set
+            {
+                image_url = value;
+                OnPropertyChanged(nameof(Image_url));
             }
         }
 
@@ -87,18 +114,90 @@ namespace LTTQ_DoAn.ViewModel
         {
             CancelCommand = new ViewModelCommand(ExecuteCancelCommand, CanExecuteCancelCommand);
             ConfirmChangeCommand = new ViewModelCommand(ExecuteChangeCommand, CanExecuteChangeCommand);
+            AddImageCommand = new ViewModelCommand(ExecuteAddImageCommand, CanExecuteAddImageCommand);
 
+        }
+        public int convertBacsiSub_ID(string inputString)
+        {
+            // Tách chuỗi sử dụng phương thức Split
+            string[] parts = inputString.Split(new[] { ':' }, 2);
+            string k1 = parts[0].Substring(1);
+            return int.Parse(k1);
+        }
+        private void update()
+        {
+            KHOA updateKhoa = (from m in _db.KHOA
+                                       where m.MAKHOA == Khoa.MAKHOA
+                                       select m).Single();
+            updateKhoa.TENKHOA = Khoa.TENKHOA;
+            updateKhoa.NGAYTHANHLAP = Khoa.NGAYTHANHLAP;
+            if (Bacsi != null)
+            {
+                updateKhoa.TRUONGKHOA = convertBacsiSub_ID(Bacsi);
+            }
+            updateKhoa.PICTURE = Image_url;
+            _db.SaveChanges();
         }
         public ChangeFieldViewModel(KHOA SelectedKhoa)
         {
             Khoa = SelectedKhoa;
             loadBacsi();
             FindTruongKhoa();
+            Image_url = SelectedKhoa.PICTURE;
             CancelCommand = new ViewModelCommand(ExecuteCancelCommand, CanExecuteCancelCommand);
             ConfirmChangeCommand = new ViewModelCommand(ExecuteChangeCommand, CanExecuteChangeCommand);
+            AddImageCommand = new ViewModelCommand(ExecuteAddImageCommand, CanExecuteAddImageCommand);
 
         }
+        private bool CanExecuteAddImageCommand(object? obj)
+        {
+            return true; //ko điều kiện
+        }
+        private void postImage(string path)
+        {
+            var client = new RestClient("http://3.25.245.200");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+            var request = new RestRequest("photo/item");
+            request.AddFile("image", path);
+            var response = client.Post(request);
+            var content = response.Content; // Raw content as string
+            ImageResponse jsonResponse = JsonNet.Deserialize<ImageResponse>(content);
+            string data = jsonResponse.url.ToString();
+            Image_url = data;
+        }
+        private void ExecuteAddImageCommand(object? obj)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image files|*.bmp;*.jpg;*.png";
+            ofd.FilterIndex = 1;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Image = new BitmapImage(new Uri(ofd.FileName));
+            }
+            string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+            //MessageBox.Show(currentDirectory);
+            //string direct = @"D:\\Workspace\\Learn\\TestGitDoAn\\image.png";
+            string store_dir = currentDirectory + "\\field_temporary_image.png";
+            BitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(Image));
 
+            using (var fileStream = new System.IO.FileStream(store_dir, System.IO.FileMode.Create))
+            {
+                encoder.Save(fileStream);
+            }
+            try
+            {
+                postImage(store_dir);
+                new MessageBoxCustom("Thông báo", "Tải ảnh thành công", MessageType.Success, MessageButtons.OK).ShowDialog();
+
+            }
+
+            catch (Exception e)
+            {
+                new MessageBoxCustom("Lỗi", "Tải ảnh thất bại\n Lỗi: " + e.Message, MessageType.Error, MessageButtons.OK).ShowDialog();
+            }
+
+        }
         private bool CanExecuteCancelCommand(object? obj)
         {
             return true; //ko điều kiện
@@ -113,8 +212,20 @@ namespace LTTQ_DoAn.ViewModel
         }
         private void ExecuteChangeCommand(object? obj)
         {
-            //
-            System.Windows.Application.Current.MainWindow.Close();
+            try
+            {
+                update();
+                //MessageBox.Show("Sửa thông tin y sĩ thành công!");
+                new MessageBoxCustom("Thành công", "Sửa thông tin khoa thành công!", MessageType.Success, MessageButtons.OK).ShowDialog();
+                System.Windows.Application.Current.MainWindow.Close(); // sau khi thêm sẽ đóng cửa sổ
+
+            }
+            catch (Exception err)
+            {
+                new MessageBoxCustom("Lỗi", err.Message, MessageType.Error, MessageButtons.OKCancel).ShowDialog();
+                //MessageBox.Show(err.Message);
+                System.Windows.Application.Current.MainWindow.Close(); // sau khi thêm sẽ đóng cửa sổ
+            }
         }
     }
 }
