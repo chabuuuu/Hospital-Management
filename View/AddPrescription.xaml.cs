ca511d5ffb11fabbd6146eae5c5ea9ecab46bc0b
Tam-Vu <vuthanhtam12062003@gmail.com>
2024-01-16 02:06:28 +0700

    thanh cuon ghi chu don thuoc

--- View/AddPrescription.xaml.cs
+++ View/AddPrescription.xaml.cs
@@ -33,5 +33,12 @@ namespace LTTQ_DoAn.View
                 Prescription.CaretIndex = Prescription.Text.Length;
             }
         }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scv = (ScrollViewer)sender;
+            scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+            e.Handled = true;
+        }
     }
 }
